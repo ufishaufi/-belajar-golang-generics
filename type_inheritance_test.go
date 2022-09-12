573b5db7405288fb9b1718390735fd1924951979
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-09-12 21:10:39 +0800

    create Type Parameter Inheritance

---
+++ type_inheritance_test.go
@@ -0,0 +1,54 @@
+package belajar_golang_generics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Employee interface {
+	GetName() string
+}
+
+func GetName[T Employee](parameter T) string {
+	return parameter.GetName()
+}
+
+type Manager interface {
+	GetName() string
+	GetManagerName() string
+}
+
+type MyManager struct {
+	Name string
+}
+
+func (m *MyManager) GetName() string {
+	return m.Name
+}
+
+func (m *MyManager) GetManagerName() string {
+	return m.Name
+}
+
+type VicePresident interface {
+	GetName() string
+	GetVicePresidentName() string
+}
+
+type MyVicePresident struct {
+	Name string
+}
+
+func (m *MyVicePresident) GetName() string {
+	return m.Name
+}
+
+func (m *MyVicePresident) GetVicePresidentName() string {
+	return m.Name
+}
+
+func TestGetName(t *testing.T) {
+	assert.Equal(t, "Shaufi", GetName[Manager](&MyManager{Name: "Shaufi"}))
+	assert.Equal(t, "Shaufi", GetName[VicePresident](&MyVicePresident{Name: "Shaufi"}))
+}
