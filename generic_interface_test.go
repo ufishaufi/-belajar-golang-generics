ba7f202be243958f8388abb51f3a3f91133a70f9
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-09-19 13:50:20 +0800

    create Generic Interface

---
+++ generic_interface_test.go
@@ -0,0 +1,38 @@
+package belajar_golang_generics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type GetterSetter[T any] interface {
+	GetValue() T
+	SetValue(value T)
+}
+
+func ChangeValue[T any](param GetterSetter[T], value T) T {
+	param.SetValue(value)
+	return param.GetValue()
+}
+
+type MyData[T any] struct {
+	Value T
+}
+
+func (d *MyData[T]) GetValue() T {
+	return d.Value
+}
+
+func (d *MyData[T]) SetValue(value T) {
+	d.Value = value
+}
+
+func TestGenericInterface(t *testing.T) {
+	myData := MyData[string]{}
+
+	result := ChangeValue[string](&myData, "Shaufi")
+
+	assert.Equal(t, "Shaufi", result)
+	assert.Equal(t, "Shaufi", myData.Value)
+}
